c540f2005c83666c836cb913d2f440f9a244e124
sahil242bhatiya <sahil242bhatiya@gmail.com>
2024-04-21 23:32:14 +0530

    new

--- quick_action.php
+++ quick_action.php
@@ -131,10 +131,25 @@ if (isset($fname) && trim($fname) !== '' && isset($email) && trim($email) !== ''
 
                     $resend = Resend::client('re_awQ3YTAE_8sZwNMMVR4YzwTbd5FUr1fvS');
 
-                    $resend->emails->send([
+                    $resend->batch->send([
                         'from' => 'onboarding@resend.dev',
-                        'to' => 'it_dev@gapollo.net',
+                        'to' => ['it_dev@gapollo.net'],
                         'cc' => ['info@apollo.co.in','arjun_patel@gapollo.net','dcbrainsinquiry@gmail.com','info@apollo.co.in'],
+                        'subject' => 'New Enquiry From Gujarat Apollo Corporate Website',
+                        'html' => $message_body
+                    ],[
+                        'from' => 'onboarding@resend.dev',
+                        'to' => ['info@apollo.co.in'],
+                        'subject' => 'New Enquiry From Gujarat Apollo Corporate Website',
+                        'html' => $message_body
+                    ],[
+                        'from' => 'onboarding@resend.dev',
+                        'to' => ['arjun_patel@gapollo.net'],
+                        'subject' => 'New Enquiry From Gujarat Apollo Corporate Website',
+                        'html' => $message_body
+                    ],[
+                        'from' => 'onboarding@resend.dev',
+                        'to' => ['dcbrainsinquiry@gmail.com'],
                         'subject' => 'New Enquiry From Gujarat Apollo Corporate Website',
                         'html' => $message_body
                     ]);
