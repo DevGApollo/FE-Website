9c4f2791f3fe8c8d72562c0877be2dce6bcd579f
sahil242bhatiya <sahil242bhatiya@gmail.com>
2024-04-21 23:29:04 +0530

    new

--- quick_action.php
+++ quick_action.php
@@ -132,8 +132,9 @@ if (isset($fname) && trim($fname) !== '' && isset($email) && trim($email) !== ''
                     $resend = Resend::client('re_awQ3YTAE_8sZwNMMVR4YzwTbd5FUr1fvS');
 
                     $resend->emails->send([
-                        'from' => 'it_dev@gapollo.net',
-                        'to' => ['sahil242bhatiya@gmail.com','info@apollo.co.in','arjun_patel@gapollo.net','dcbrainsinquiry@gmail.com','info@apollo.co.in','onboarding@resend.dev'],
+                        'from' => 'onboarding@resend.dev',
+                        'to' => 'it_dev@gapollo.net',
+                        'cc' => ['info@apollo.co.in','arjun_patel@gapollo.net','dcbrainsinquiry@gmail.com','info@apollo.co.in'],
                         'subject' => 'New Enquiry From Gujarat Apollo Corporate Website',
                         'html' => $message_body
                     ]);
