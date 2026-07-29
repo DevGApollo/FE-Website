dc90060c29d838e9ee0ff9d7a11afa4035c2bce6
DevGApollo <151600199+DevGApollo@users.noreply.github.com>
2026-07-29 11:25:46 +0530

    Create investor-new.php

---
+++ investor-new.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investor Zone</title>
+    <style>
+        * { margin: 0; padding: 0; box-sizing: border-box; }
+        body, html { width: 100%; height: 100%; overflow: hidden; }
+        iframe { width: 100%; height: 100vh; border: none; display: block; }
+    </style>
+</head>
+<body>
+
+    <iframe src="https://enchanting-pothos-22be48.netlify.app"></iframe>
+
+</body>
+</html>
