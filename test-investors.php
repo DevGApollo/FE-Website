985b7faef202979228924c5ec4a42cbbf37c6b36
DevGApollo <151600199+DevGApollo@users.noreply.github.com>
2026-07-29 11:29:24 +0530

    Add files via upload

---
+++ test-investors.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investor Zone Test</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            width: 100vw;
+            overflow: hidden;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+
+    <iframe src="https://enchanting-pothos-22be48.netlify.app"></iframe>
+
+</body>
+</html>
